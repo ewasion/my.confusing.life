894f65df346466ed738c26b2e4c2dd289153d607
Danny Void <danielilluminatic@gmail.com>
2017-04-18 22:35:29 -0500

    v 1.0.1
    - added quotations
    - cleaned up php

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
-if (empty($_GET)) {
+
 $f_contents = file("flatfile.txt"); 
+
+if (empty($_GET)) {
 $rand_line = $f_contents[rand(0, count($f_contents) - 1)];
 $rand_line = trim(preg_replace('/\s\s+/', ' ', $rand_line));
 }
 
 else {
 $line = (int)$_GET['quote'];
-$f_contents = file("flatfile.txt"); 
-$lineRead = $f_contents[$line - 1];
-$rand_line = $lineRead;
+$rand_line = $f_contents[$line - 1];
 $rand_line = trim(preg_replace('/\s\s+/', ' ', $rand_line));
 }
+
 ?>
 <title><?php echo $rand_line;?></title>
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <div class="quote">
-  <div onclick="location.href='http://dannyvoid.design/';"><?php echo $rand_line;?></div>
+	"<br /><br />&nbsp;&nbsp;&nbsp;
+	<div onclick="location.href='http://dannyvoid.design/';"><?php echo $rand_line;?></div>
+	<br /><br /><br />&nbsp;&nbsp;&nbsp;"
 </div>
